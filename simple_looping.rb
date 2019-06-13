afd0035c3993a9f03675e2cfca424d9362cffb84
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-13 18:21:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,7 @@ def loop_iterator(number_of_times)
   counter = 0 
   loop do 
     puts "Welcome to Flatiron School's Web Development Course!"
-    if  counter >=7
+    
       break
     end 
     
