934c154460ccd1cb8de3388e97ea54689a3440c1
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-13 18:24:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
-  
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
   loop do
     puts phrase
